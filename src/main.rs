0af9e1c34ecf8c1cbd0c8f082888740d20ab09b1
WANG Xuerui <git@xen0n.name>
2022-06-08 15:42:45 +0800

    feat: check a few more insns

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    println!("{:?}", disas_riscv_insn_32bit(0x000fc397));
+    macro_rules! d {
+        ($x: expr) => {
+            println!("{:?}", disas_riscv_insn_32bit($x));
+        }
+    }
+
+    d!(0x000fc397);
+    d!(0x41c30333);
+    d!(0x4c03be03);
+    d!(0x00135313);
+    d!(0x000e0067);
 }
 
 #[derive(Debug)]
