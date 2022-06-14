0c5e25764bbdeb1d3899049194d73dd0c2098376
WANG Xuerui <git@xen0n.name>
2022-06-14 13:25:24 +0800

    feat: change example to hello world

--- src/bin/larva-test/main.rs
+++ src/bin/larva-test/main.rs
@@ -2,42 +2,26 @@ use larva::exec;
 
 fn main() {
     /*
-    00000000000100e8 <fib>:
-       100e8:       4785                    li      a5,1
-       100ea:       02a7d563                bge     a5,a0,10114 <fib+0x2c>
-       100ee:       1101                    addi    sp,sp,-32
-       100f0:       ec06                    sd      ra,24(sp)
-       100f2:       e822                    sd      s0,16(sp)
-       100f4:       e426                    sd      s1,8(sp)
-       100f6:       842a                    mv      s0,a0
-       100f8:       3579                    addiw   a0,a0,-2
-       100fa:       fefff0ef                jal     ra,100e8 <fib>
-       100fe:       84aa                    mv      s1,a0
-       10100:       fff4051b                addiw   a0,s0,-1
-       10104:       fe5ff0ef                jal     ra,100e8 <fib>
-       10108:       9d25                    addw    a0,a0,s1
-       1010a:       60e2                    ld      ra,24(sp)
-       1010c:       6442                    ld      s0,16(sp)
-       1010e:       64a2                    ld      s1,8(sp)
-       10110:       6105                    addi    sp,sp,32
-       10112:       8082                    ret
-       10114:       4505                    li      a0,1
-       10116:       8082                    ret
+    00000000000100b0 <_start>:
+           100b0:       4505                    li      a0,1
+           100b2:       00000597                auipc   a1,0x0
+           100b6:       01e58593                addi    a1,a1,30 # 100d0 <hello>
+           100ba:       4631                    li      a2,12
+           100bc:       04000893                li      a7,64
+           100c0:       00000073                ecall
+           100c4:       00000513                li      a0,0
+           100c8:       05d00893                li      a7,93
+           100cc:       00000073                ecall
 
-    0000000000010118 <_start>:
-       10118:       1141                    addi    sp,sp,-16
-       1011a:       e406                    sd      ra,8(sp)
-       1011c:       452d                    li      a0,11
-       1011e:       fcbff0ef                jal     ra,100e8 <fib>
-       10122:       05d00893                li      a7,93
-       10126:       00000073                ecall
-             */
-    let mem: [u8; 66] = [
-        0x85, 0x47, 0x63, 0xd5, 0xa7, 0x02, 0x01, 0x11, 0x06, 0xec, 0x22, 0xe8, 0x26, 0xe4, 0x2a,
-        0x84, 0x79, 0x35, 0xef, 0xf0, 0xff, 0xfe, 0xaa, 0x84, 0x1b, 0x05, 0xf4, 0xff, 0xef, 0xf0,
-        0x5f, 0xfe, 0x25, 0x9d, 0xe2, 0x60, 0x42, 0x64, 0xa2, 0x64, 0x05, 0x61, 0x82, 0x80, 0x05,
-        0x45, 0x82, 0x80, 0x41, 0x11, 0x06, 0xe4, 0x2d, 0x45, 0xef, 0xf0, 0xbf, 0xfc, 0x93, 0x08,
-        0xd0, 0x05, 0x73, 0x00, 0x00, 0x00,
+    00000000000100d0 <hello>:
+           100d0:       6c6c6568                .word   0x6c6c6568
+           100d4:       6f77206f                .word   0x6f77206f
+           100d8:       0a646c72                .word   0x0a646c72
+         */
+    let mem: [u8; 44] = [
+        0x05, 0x45, 0x97, 0x05, 0x00, 0x00, 0x93, 0x85, 0xe5, 0x01, 0x31, 0x46, 0x93, 0x08, 0x00,
+        0x04, 0x73, 0x00, 0x00, 0x00, 0x13, 0x05, 0x00, 0x00, 0x93, 0x08, 0xd0, 0x05, 0x73, 0x00,
+        0x00, 0x00, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x0a,
     ];
 
     let mut state = exec::RvIsaState::default();
@@ -50,7 +34,7 @@ fn main() {
     executor.stack(4096).unwrap();
 
     let block_addr = mem.as_ptr() as u64;
-    let entry_pc = block_addr + 48;
+    let entry_pc = block_addr + 0;
     println!("code addr = {:016x}", block_addr);
     println!(" entry pc = {:016x}", entry_pc);
     let exit_reason = executor.exec(entry_pc);
